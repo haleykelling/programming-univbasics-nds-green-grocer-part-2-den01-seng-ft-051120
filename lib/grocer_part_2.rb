16841b820d1efafee94a31e17806fc4cba46642c
Haley Kelling <haley@kellings.com>
2020-05-04 21:19:04 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -43,4 +43,12 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  cart_with_coupons = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(cart_with_coupons)
+  final_cart.each do |item|
+    item_price_total = item[:price] * item[:count]
+    total_price += item_price_total
+  end
+  total_price
 end
